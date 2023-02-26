13491420687478f33af3b13888cd3c8881b43297
CrioUser <crio-user@criodo.com>
2023-02-26 08:23:35 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://15.206.42.12:8082" };
+const config = { backendEndpoint: "http://3.109.161.178:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -14,10 +14,15 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-    // let response = await fetch('http://65.2.46.88:8082/cities');
+    // let response = await fetch('http://15.206.42.12:8082/cities');
+    try{ 
     let response = await fetch(`${config.backendEndpoint}/cities`);
     let data = await response.json();
     return data
+    }catch(Err){
+      return null;
+    }
+   
 }
 
 //Implementation of DOM manipulation to add cities
@@ -26,7 +31,7 @@ function addCityToDOM(id, city, description, image) {
   // 1. Populate the City details and insert those details into the DOM
   let dataId = document.getElementById("data");
   dataId.innerHTML += `<div class="col-sm-6 col-lg-3 mb-4">
-  <a href="/pages/adventures/?city = ${id}">
+  <a href="/pages/adventures/?city=${id}">
     <div class="tile" id = ${id}>
       <div class="tile-text text-center">
         <h5>${city}</h5>
